704264c8fb50a9e7cc35089e8643e1bcfe36aabe
Jocimar Lopes <45192419+jocimarlopes@users.noreply.github.com>
2019-05-03 12:27:56 -0300

    Update pomodoro-app.py

--- pomodoro-app.py
+++ pomodoro-app.py
@@ -5,14 +5,13 @@ import tkinter as tk
 
 frequency = 500
 duration = 10000
-play = input("Deseja iniciar?")
+play = 0
 
-while play in 'sim Sim SIM YES yes Yes':
-    input("Pressione ENTER para iniciar 25 minutos..")
-
+while play == 0:
     counter = 1
 
     while counter < 4:
+        input("Pressione ENTER para iniciar 25 minutos..")
         time.sleep(1)
         print('Pomodori (25 minutos) a partir de agora!')
         time.sleep(1500)
@@ -20,12 +19,16 @@ while play in 'sim Sim SIM YES yes Yes':
         time.sleep(1)
         print('Pausa curta (05 minutos)')
         time.sleep(1)
-        input('Pressione enter para Começar')
+        input('Pressione enter para iniciar a Pausa Curta..')
         time.sleep(300)
+        winsound.Beep(600, 5000)
         counter = counter + 1
     else:
+        print('Pausa Longa (15 minutos)')
+        input('Pressione ENTER para iniciar a Pausa Longa..')
         time.sleep(300)
         time.sleep(600)
+        winsound.Beep(600, 5000)
 else:
     print("Finalizando...")
     time.sleep(2)
