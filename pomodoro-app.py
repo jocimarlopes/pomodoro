63c7b647e2809628ee916b2e70ba3d5cee553bc5
jocimarlopes <lopes.jocimar@gmail.com>
2020-03-25 01:08:17 -0300

    ##Modificação de aplicação #####Versão: 0.2

--- pomodoro-app.py
+++ pomodoro-app.py
@@ -1,13 +1,13 @@
-import winsound
 import time
 import sys
-import tkinter as tk
 
-frequency = 500
-duration = 10000
 play = 0
 
 while play == 0:
+    print("APLICAÇÃO SEM SOM (BETA)")
+    time.sleep(1)
+    print("Versão: 0.2")
+    time.sleep(1)
     counter = 1
 
     while counter < 4:
@@ -15,20 +15,17 @@ while play == 0:
         time.sleep(1)
         print('Pomodori (25 minutos) a partir de agora!')
         time.sleep(1500)
-        winsound.Beep(600, 5000)
         time.sleep(1)
         print('Pausa curta (05 minutos)')
         time.sleep(1)
         input('Pressione enter para iniciar a Pausa Curta..')
         time.sleep(300)
-        winsound.Beep(600, 5000)
         counter = counter + 1
     else:
         print('Pausa Longa (15 minutos)')
         input('Pressione ENTER para iniciar a Pausa Longa..')
         time.sleep(300)
         time.sleep(600)
-        winsound.Beep(600, 5000)
 else:
     print("Finalizando...")
     time.sleep(2)
